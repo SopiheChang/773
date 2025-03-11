9f67dde95a292b7f56bf00eb156cb09d140921d7
SopiheChang <nick41212@gmail.com>
2025-03-11 22:01:30 +0800

    Update app.py

--- app.py
+++ app.py
@@ -11,10 +11,11 @@ from linebot.v3.messaging.models import FlexMessage
 
 app = Flask(__name__)
 
-messaging_api = MessagingApi(LINE_CHANNEL_ACCESS_TOKEN)  # ✅ 添加这一行
+
 LINE_CHANNEL_ACCESS_TOKEN = os.getenv("LINE_CHANNEL_ACCESS_TOKEN")
 LINE_CHANNEL_SECRET = os.getenv("LINE_CHANNEL_SECRET")
 GITHUB_FILE_URL = "https://raw.githubusercontent.com/SopiheChang/773/main/data.xlsx"
+messaging_api = MessagingApi(LINE_CHANNEL_ACCESS_TOKEN)  # ✅ 添加这一行
 
 line_bot_api = LineBotApi(LINE_CHANNEL_ACCESS_TOKEN)
 handler = WebhookHandler(LINE_CHANNEL_SECRET)
